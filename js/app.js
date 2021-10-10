31bc76afb558dd0ce1de70dcb1942d8537e8a881
asaduzzaman2189 <asaduzzaman2189@gmail.com>
2021-10-10 17:21:14 +0600

    36_5-2 Display temperature, city, dynamic weather icon

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 const API_KEY = `cd17c937be6fa27dcd43367af2862885`;
 const searchTemperature = () => {
   const city = document.getElementById("city-name").value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayTemperature(data));
 };
 
+const setInnerText = (id, text) => {
+  document.getElementById(id).innerText = text;
+};
+
 const displayTemperature = (temperature) => {
-  console.log(temperature);
+  setInnerText("city", temperature.name);
+  setInnerText("temperature", temperature.main.temp);
+  setInnerText("condition", temperature.weather[0].main);
+  //set weather icon
+  const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+  const imgIcon = document.getElementById("weather-icon");
+  imgIcon.setAttribute("src", url); // src এর ভিতরে url এভাবে দিতে হয়।
 };
